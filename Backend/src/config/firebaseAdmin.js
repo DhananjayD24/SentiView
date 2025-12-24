36ab44ca0df2a88ad85a79ac6491e22c3a739c85
DhananjayD24 <dcube2004@gmail.com>
2025-12-24 19:12:56 +0530

    Refactor Firebase Admin initialization to support environment variable for service account key

--- Backend/src/config/firebaseAdmin.js
+++ Backend/src/config/firebaseAdmin.js
@@ -1,9 +1,21 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("serviceAccountKey.json", "utf-8")
-);
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync("serviceAccountKey.json", "utf-8")
+// );
+
+let serviceAccount;
+
+if (process.env.SERVICE_ACCOUNT_KEY) {
+  // ✅ Production (Render)
+  serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+} else {
+  // ✅ Local development
+  serviceAccount = JSON.parse(
+    fs.readFileSync("serviceAccountKey.json", "utf-8")
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
